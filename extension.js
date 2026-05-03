0b181811de579255fb2e5604fe78d2d3bfb78e91
Emmeken Faun Snelder <git@emmeken.net>
2026-05-03 02:27:11 +0200

    working!

--- extension.js
+++ extension.js
@@ -2,10 +2,8 @@ import Gio from 'gi://Gio';
 import GLib from 'gi://GLib';
 import { Extension } from 'resource:///org/gnome/shell/extensions/extension.js';
 
-Gio._promisify(Gio.File.prototype, 'read_async');
-Gio._promisify(Gio.File.prototype, 'replace_contents_async');
-Gio._promisify(Gio.InputStream.prototype, 'read_bytes_async', 'read_finish');
-Gio._promisify(Gio.InputStream.prototype, 'close_async');
+Gio._promisify(Gio.File.prototype, 'load_contents_async');
+Gio._promisify(Gio.File.prototype, 'replace_contents_bytes_async', 'replace_contents_finish');
 
 function handler(m) {
   return (e)=>{
@@ -26,7 +24,7 @@ export default class TransMyGnome extends Extension {
 
       const oldUserGtkStylesheet = await this.readFileContents(this.userGtkStylesheetFile).catch(handler("Could not get gtk.css contents."))
       const newUserGtkStylesheet = this.addTransMyGnomeImport(oldUserGtkStylesheet)
-      await this.replaceFileContents(this.userGtkStylesheetFile,newUserGtkStylesheet).catch(handler("Could not replace gtk.css contents."))
+      await this.replaceFileContents(this.userGtkStylesheetFile,newUserGtkStylesheet)
     }
     catch (e) {
       console.error(e)
@@ -37,29 +35,29 @@ export default class TransMyGnome extends Extension {
     try {
       const oldUserGtkStylesheet = await this.readFileContents(this.userGtkStylesheetFile).catch("Could not get gtk.css contens.")
       const newUserGtkStylesheet = this.removeTransMyGnomeImport(oldUserGtkStylesheet)
-      await this.replaceFileContents(this.userGtkStylesheetFile,newUserGtkStylesheet).catch(handler("Could not replace gtk.css contents."))
+      await this.replaceFileContents(this.userGtkStylesheetFile,newUserGtkStylesheet)
     }
     catch (e) {
       console.error(e)
     }
   }
 
-  addLine(line,contents) { return contents + (/\n$/.test(contents)) ? line : '\n' + line }
-  removeLine(line,contents) { return contents.replace(new RegExp("\n?"+line), "") }
-
   async readFileContents(file) {
     let string = ''
     if (file.query_exists(null)) {
-      const stream = await file.read_async(GLib.PRIORITY_DEFAULT, null).catch(handler(`Could not open file for reading ${file.get_path()}`))
-      const bytes = await stream.read_bytes_async(4096, GLib.PRIORITY_DEFAULT, null).catch(handler(`Could note read file ${file.get_path()}`))
-      stream.close_async(null)
-      string = bytes.toString()
+      const [contents] = await file.load_contents_async(null).catch(handler(`Could not open file for reading ${file.get_path()}`))
+      string = new TextDecoder().decode(contents);
     }
     return string
   }
 
   async replaceFileContents(file,contents) {
-    await file.replace_contents_async(new GLib.Bytes(contents), null, true, null, null).catch(handler(`Failed to replace contents of a file '${file.get_path()}'.`))
+    try {
+      await file.replace_contents_bytes_async(new GLib.Bytes(contents), null, false, Gio.FileCreateFlags.REPLACE_DESTINATION, null)
+
+    } catch (e) {
+      handler(`Failed to replace contents of file '${file.get_path()}'.`)(e)
+    }
   }
 
   async setupGtkFolder() {
@@ -68,16 +66,17 @@ export default class TransMyGnome extends Extension {
     }
   }
 
-  addTransMyGnomeImport(string) {
-    const regex = /^.*trans-my-gnome.*$/m
-    const importString = `@import ('${this.transMyGnomeStylesheetFile.get_path()}');`;
-    return string + (regex.test(string)) ? '' : this.addLine(importString, string)
+  addTransMyGnomeImport(contents) {
+    const removedImport = this.removeTransMyGnomeImport(contents)
+    const importString = `@import '${this.transMyGnomeStylesheetFile.get_path()}';`;
+    const addedImport = removedImport + importString
+    return addedImport
   }
 
-  removeTransMyGnomeImport(string) {
-    const regex = /^.*trans-my-gnome.*$/m
-    const toRemove = regex.exec(string)[0]
-    return removeLine(toRemove,string)
+  removeTransMyGnomeImport(contents) {
+    const regex = /@import.*?trans-my-gnome@emmeken\.net.*?;/m
+    const removedImport = contents.replace(regex,'')
+    return removedImport
   }
 
 }
